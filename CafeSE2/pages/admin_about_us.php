9d6b1e40250a48430a30fd924058a34e050f9586
JanVincentDyogi <janvincentdyogi@gmail.com>
2024-03-07 20:52:17 +0800

    added socials for admin

--- CafeSE2/pages/admin_about_us.php
+++ CafeSE2/pages/admin_about_us.php
@@ -92,7 +92,45 @@ include 'connect.php';
                 </tbody>
             </table>
         </div>
-
+        <h2>Socials</h2>
+        <div class="table-container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Content ID</th>
+                        <th scope="col">Social Media</th>
+                        <th scope="col">Social Media Link</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $content_query = "SELECT * FROM Content WHERE content_ID IN (10, 11)";
+                    $content_result = mysqli_query($con, $content_query);
+                    if ($content_result) {
+                        while ($row = mysqli_fetch_assoc($content_result)) {
+                            $content_id = $row['content_ID'];
+                            $content_text = $row['content_text'];
+                            $social_media_name = "";
+                            if ($content_id == 10) {
+                                $social_media_name = "Facebook";
+                            } elseif ($content_id == 11) {
+                                $social_media_name = "Instagram";
+                            }
+                            echo '<tr>
+                            <th scope="row">' . $content_id . '</th>
+                            <td>' . $social_media_name . '</td>
+                            <td>' . $content_text . '</td>
+                            <td>
+                                <button class="btn btn-primary"><a href="about_us_operations/update_content_text.php?content_id=' . $content_id . '" class="text-light">Update</a></button>
+                            </td>
+                        </tr>';
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
 
