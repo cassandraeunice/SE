595ec2d6ee34802f114ba14d1fd5fc676505c731
Cassandra Cortez <cassandraeunice.cortez.cics@ust.edu.ph>
2024-03-20 23:01:06 +0800

    fixed home and about us page

--- CafeSE2/pages/home_operations/update_content.php
+++ CafeSE2/pages/home_operations/update_content.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Update Content</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/update-content.css">
     <script>
         function confirmUpdate() {
             var result = confirm("Are you sure you want to update?");
@@ -53,6 +53,7 @@ if(isset($_POST['submit'])){
 
 <body>
     <div class="container my-5">
+    <a href="../admin_home.php" class="btn btn-secondary"><svg width="36px" height="36px" viewBox="0 0 1024 1024" fill="#FFF3E2" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#FFF3E2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill=""></path></g></svg></a>
         <h2>Update Content</h2>
         <form method="post" enctype="multipart/form-data" onsubmit="confirmUpdate()">
             <div class="mb-3">
@@ -61,10 +62,11 @@ if(isset($_POST['submit'])){
             </div>
             <div class="mb-3">
                 <label>Content Image</label>
-                <input type="file" class="form-control" name="content_image">
+                <input type="file" class="custom-file-input" name="content_image">
             </div>
-            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-            <a href="../admin_home.php" class="btn btn-secondary">Back</a>
+            <div class="button-container">
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+            </div>
         </form>
     </div>
 </body>
--- CafeSE2/pages/home_operations/update_content_image.php
+++ CafeSE2/pages/home_operations/update_content_image.php
@@ -31,7 +31,7 @@ if(isset($_POST['submit'])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Update Image</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/update-content-image.css">
     <script>
         function confirmUpdate() {
             var result = confirm("Are you sure you want to update?");
@@ -42,13 +42,15 @@ if(isset($_POST['submit'])){
 
 <body>
     <div class="container my-5">
+        <a href="../admin_home.php" class="btn btn-secondary"><svg width="36px" height="36px" viewBox="0 0 1024 1024" fill="#FFF3E2" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#FFF3E2"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill=""></path></g></svg></a>
         <h2>Update Image</h2>
         <form method="post" enctype="multipart/form-data" onsubmit="return confirmUpdate()">
             <div class="mb-3">
-                <input type="file" class="form-control" name="content_image" required>
+                <input type="file" class="custom-file-input" name="content_image" required>
             </div>
-            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-            <a href="../admin_home.php" class="btn btn-secondary">Back</a>
+            <div class="button-container">
+                <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+            </div>
         </form>
     </div>
 </body>
