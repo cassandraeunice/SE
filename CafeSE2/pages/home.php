f6402ec7ed4c3d9e881749960ffbffa345ee6ea3
MiguelBalagapo <juanmiguel.balagapo.cics@ust.edu.ph>
2024-04-07 07:56:46 +0800

    fix

--- CafeSE2/pages/home.php
+++ CafeSE2/pages/home.php
@@ -170,6 +170,7 @@ while ($row = mysqli_fetch_assoc($content_result)) {
   </footer>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         var carousel = document.getElementById('carouselFade');
