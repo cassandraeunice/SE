c7507d732f1bfb16f15b30ce2c9787c5ebb7b297
LuisDalisay <124044185+LuisDalisay@users.noreply.github.com>
2024-04-12 10:52:14 +0800

    centered error msg
    credits to jm

--- CafeSE2/pages/category.php
+++ CafeSE2/pages/category.php
@@ -116,9 +116,9 @@ if (isset($_GET['category_id'])) {
     <section>
       <div class="display-container">
         <?php foreach ($subcategory_products as $subcategory_name => $products) : ?>
-          <div class="menu<?php echo empty($subcategory_name)|| count($products) === 0 ? ' no-subcategory' : ''; ?>">
+          <div class="menu<?php echo empty($subcategory_name) || count($products) === 0 ? ' no-subcategory' : ''; ?>">
 
-    <?php if (!empty($subcategory_name)) : ?>
+            <?php if (!empty($subcategory_name)) : ?>
               <div class="heading">
                 <h2><?php echo $subcategory_name; ?></h2>
               </div>
